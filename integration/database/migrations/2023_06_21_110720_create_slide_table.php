660756ea11586439d65d6b60f8bf1188736a634d
Adiel Drystan <kinberlintchamba2003@gmail.com>
2023-06-21 12:07:47 +0100

    migrate sellers,slide,status,subscribers

---
+++ integration/database/migrations/2023_06_21_110720_create_slide_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slide', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->text('src');
+            $table->string('texte', 60);
+            $table->string('min', 20);
+            $table->timestamp('createdat')->nullable()->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slide');
+    }
+};
---
+++ integration/database/migrations/2023_06_21_110720_create_subscribers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('user')->index('subscribers_ibfk_2');
+            $table->integer('channel')->index('subscribers_ibfk_1');
+            $table->timestamp('createdat')->nullable()->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscribers');
+    }
+};
