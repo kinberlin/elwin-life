c9088b40435d5e2461e83ac1187d0173aa30c1fe
Adiel Drystan <kinberlintchamba2003@gmail.com>
2023-06-21 12:11:16 +0100

    end of migrations with : wishlist,wishlist_items

---
+++ integration/database/migrations/2023_06_21_111009_create_wishlist_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlist_items', function (Blueprint $table) {
+            $table->integer('wishlist_id')->index('wishlist_id');
+            $table->integer('product_id')->index('product_id');
+            $table->timestamp('createdat')->nullable()->useCurrent();
+            $table->integer('quantity');
+            $table->integer('id', true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlist_items');
+    }
+};
---
+++ integration/database/migrations/2023_06_21_111009_create_wishlist_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlist', function (Blueprint $table) {
+            $table->integer('wishlist_id', true);
+            $table->integer('customer_id')->unique('customer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlist');
+    }
+};
