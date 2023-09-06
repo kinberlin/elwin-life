c95530a475b80739aa4e0d09939400152aef1e9a
Adiel Drystan <kinberlintchamba2003@gmail.com>
2023-09-06 21:20:15 +0100

    testing remaining for Transaction Status Check

--- integration/app/Http/Controllers/TransactionController.php
+++ integration/app/Http/Controllers/TransactionController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Orders;
+use App\Models\Payments;
 use App\Models\Transaction;
+use Carbon\Carbon;
+use DB;
+use Exception;
 use Http;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class TransactionController extends Controller
 {
@@ -65,38 +71,83 @@ class TransactionController extends Controller
         //
     }
 
-    
+
     public function checkStatus(Request $request)
     {
-        $transactionReference = $request->input('transaction_reference');
+        try {
+            $transactionReference = $request->input('transaction_reference');
 
-        // Check the transaction status using Flutterwave API or mobile money provider's API
-        // Update the transaction status in the database
-
-        // Example code:
-        $transaction = Transaction::where('transaction_reference', $transactionReference)->first();
-
-        if ($transaction) {
-            // Get transaction status using API and update the record in the database
+            // Check the transaction status using Flutterwave API or mobile money provider's API
+            // Update the transaction status in the database
 
             // Example code:
-            $newStatus = $this->getTransactiontatus($transactionReference); // Implement your API call to get the status
+            $transaction = Transaction::where('transaction_reference', $transactionReference)->first();
 
-            $transaction->status = $newStatus;
-            $transaction->save();
-
-            if ($newStatus === 'successful') {
-                // Transaction is successful
-                // Perform any necessary actions or notifications
-            } elseif ($newStatus === 'failed') {
-                // Transaction has failed
-                // Perform any necessary actions or notifications
-            } else {
-                // Transaction is still pending or unknown status
-                // Perform any necessary actions or notifications
+            if ($transaction == null) {
+                throw new Exception("Error Checking Transaction Satus for tx_ref : " . $transactionReference);
             }
-        }
+            if ($transaction != null) {
+                // Get transaction status using API and update the record in the database
+                // Example code:
+                $transacts = $this->getTransactiontatus($transactionReference); // Implement your API call to get the status
 
+                if ($transacts->data->status === 'successful') {
+                    // Transaction is successful
+                    
+                    if ($transaction->bundle != null) {
+                        DB::beginTransaction();
+                        $pay = new Payments();
+                        $pay->tx_ref = $transacts->data->tx_ref;
+                        $pay->amount = $transacts->data->amount;
+                        $pay->currency = $transacts->data->currency;
+                        $pay->status = $transacts->data->status;
+                        $pay->payment_type = $transacts->data->payment_type;
+                        $pay->flw_ref = $transacts->data->flw_ref;
+                        $pay->email = $transacts->data->customer->email;
+                        $pay->name = $transacts->data->customer->name;
+                        $pay->card_type = isset($transacts->data->card) == true ? $transacts->data->card->type : $transacts->data->payment_type;
+                        $pay->customer_id = $transacts->data->meta->consumer_mac;
+                        $pay->phone_number = $transacts->data->customer->phone_number;
+                        $pay->save();
+
+                        DB::table('Subscription')->insert([
+                            'bundle' => $transaction->data->meta->bundle,
+                            'amount' => $transaction->data->amount,
+                            'start_date' => Carbon::now()->toDateString(),
+                            'end_date' => Carbon::now()->addDays($transaction->data->meta->duration)->toDateString(),
+                            'user' => $transaction->data->meta->consumer_mac,
+                            'payment' => $pay->id
+                        ]);
+                        DB::commit();
+                    } else {
+                        DB::beginTransaction();
+                        DB::table('payments')->insert([
+                            'tx_ref' => $transacts->data->tx_ref,
+                            'amount' => $transacts->data->amount,
+                            'currency' => $transacts->data->currency,
+                            'status' => $transacts->data->status,
+                            'payment_type' => $transacts->data->payment_type,
+                            'flw_ref' => $transacts->data->flw_ref,
+                            'email' => $transacts->data->customer->email,
+                            'name' => $transacts->data->customer->name,
+                            'card_type' => $transacts->data->card->type,
+                            'customer_id' => $transacts->data->meta->consumer_mac,
+                            'order_id' => $transacts->data->meta->order_id,
+                            'phone_number' => $transacts->data->customer->phone_number
+                        ]);
+                        $ore = Orders::find($transacts->data->meta->order_id);
+                        if ($ore === null) {
+                            throw new Exception("Nous n'avons pas trouvé cette commande", 1);
+                        }
+                        $ore->status = 'Payer';
+                        $ore->save();
+                        DB::commit();
+                    }
+                }
+            }
+        } catch (Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
         // Return a response as needed
     }
 
@@ -104,7 +155,7 @@ class TransactionController extends Controller
     public function getTransactionStatus($transactionReference)
     {
         // Making a request to the Flutterwave API to get the transaction status
-        $url = "https://api.flutterwave.com/v3/transactions/verify_by_reference" ;
+        $url = "https://api.flutterwave.com/v3/transactions/verify_by_reference";
         $headers = [
             'Authorization' => 'Bearer ' . env('FLUTTERWAVE_SECRET_KEY'),
             'Content-Type' => 'application/json',
@@ -114,8 +165,8 @@ class TransactionController extends Controller
         ];
 
         $response = Http::withHeaders($headers)->withOptions($options)->get($url, [
-        'tx_ref' => $transactionReference,
-    ]);
+            'tx_ref' => $transactionReference,
+        ]);
 
         // Check if the API request was successful
         if ($response->successful()) {
@@ -123,7 +174,7 @@ class TransactionController extends Controller
             return $responseData;
             //$transacts = Transaction::where('transaction_reference',$transactionReference )->get()->first();
         }
-    
+
         // If the API request failed, you can handle the error accordingly
         // For example, you can log the error, return a default status, or throw an exception
         Log::error('Failed to retrieve transaction status from Flutterwave API');
--- integration/app/Jobs/CheckTransactionStatus.php
+++ integration/app/Jobs/CheckTransactionStatus.php
@@ -2,6 +2,11 @@
 
 namespace App\Jobs;
 
+use App\Models\Orders;
+use App\Models\Payments;
+use DB;
+use Exception;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -33,25 +38,72 @@ class CheckTransactionStatus implements ShouldQueue
         $transactions = Transaction::where('status', 'pending')->get();
 
         foreach ($transactions as $transaction) {
+            $transactionReference = $transaction->transaction_reference;
             // Check the transaction status using Flutterwave API or mobile money provider's API
-            // Update the transaction status in the database
-
+            // Delete the transaction from database if succesfull 
             // Example code:
-            $newStatus = $transaction_controller->getTransactionStatus($transaction->transaction_reference); // Implemented API call to get the Transaction
-
-            $transaction->status = $newStatus;
-            $transaction->save();
-
-            if ($newStatus === 'successful') {
-                // Transaction is successful
-                // Perform any necessary actions or notifications
-            } elseif ($newStatus=== 'failed') {
-                // Transaction has failed
-                // Perform any necessary actions or notifications
-            } else {
-                // Transaction is still pending or unknown status
-                // Perform any necessary actions or notifications
+            if ($transaction == null) {
+                throw new Exception("Error Checking Transaction Satus for tx_ref : " . $transactionReference);
             }
+            if ($transaction != null) {
+                // Get transaction status using API and update the record in the database
+                // Example code:
+                $transacts = $transaction_controller->getTransactiontatus($transactionReference); // Implement your API call to get the status
+
+                if ($transacts->data->status === 'successful') {
+                    // Transaction is successful
+                    if ($transaction->bundle != null) {
+                        DB::beginTransaction();
+                        $pay = new Payments();
+                        $pay->tx_ref = $transacts->data->tx_ref;
+                        $pay->amount = $transacts->data->amount;
+                        $pay->currency = $transacts->data->currency;
+                        $pay->status = $transacts->data->status;
+                        $pay->payment_type = $transacts->data->payment_type;
+                        $pay->flw_ref = $transacts->data->flw_ref;
+                        $pay->email = $transacts->data->customer->email;
+                        $pay->name = $transacts->data->customer->name;
+                        $pay->card_type = isset($transacts->data->card) == true ? $transacts->data->card->type : $transacts->data->payment_type;
+                        $pay->customer_id = $transacts->data->meta->consumer_mac;
+                        $pay->phone_number = $transacts->data->customer->phone_number;
+                        $pay->save();
+
+                        DB::table('Subscription')->insert([
+                            'bundle' => $transaction->data->meta->bundle,
+                            'amount' => $transaction->data->amount,
+                            'start_date' => Carbon::now()->toDateString(),
+                            'end_date' => Carbon::now()->addDays($transaction->data->meta->duration)->toDateString(),
+                            'user' => $transaction->data->meta->consumer_mac,
+                            'payment' => $pay->id
+                        ]);
+                        DB::commit();
+                    } else {
+                        DB::beginTransaction();
+                        DB::table('payments')->insert([
+                            'tx_ref' => $transacts->data->tx_ref,
+                            'amount' => $transacts->data->amount,
+                            'currency' => $transacts->data->currency,
+                            'status' => $transacts->data->status,
+                            'payment_type' => $transacts->data->payment_type,
+                            'flw_ref' => $transacts->data->flw_ref,
+                            'email' => $transacts->data->customer->email,
+                            'name' => $transacts->data->customer->name,
+                            'card_type' => $transacts->data->card->type,
+                            'customer_id' => $transacts->data->meta->consumer_mac,
+                            'order_id' => $transacts->data->meta->order_id,
+                            'phone_number' => $transacts->data->customer->phone_number
+                        ]);
+                        $ore = Orders::find($transacts->data->meta->order_id);
+                        if ($ore === null) {
+                            throw new Exception("Nous n'avons pas trouvé cette commande", 1);
+                        }
+                        $ore->status = 'Payer';
+                        $ore->save();
+                        DB::commit();
+                    }
+                }
+            }
+            // Return a response as needed
         }
     }
 
