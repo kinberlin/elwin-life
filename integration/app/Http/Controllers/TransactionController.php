c37e42aaeef4275185fce57d4b44ede643607c0d
Adiel Drystan <kinberlintchamba2003@gmail.com>
2023-09-08 18:49:17 +0100

    bug fixing for job

--- integration/app/Http/Controllers/TransactionController.php
+++ integration/app/Http/Controllers/TransactionController.php
@@ -175,7 +175,7 @@ class TransactionController extends Controller
         }
         else{
             if($response->body() != null){
-                dd($response->body());
+                dd($response->body() . 'For Transaction : '.$transactionReference);
                 $responseData = json_decode($response->body());
                 return $responseData;
             }
